1987e2b299f6d60ffcf81bad017c6a1030e40a0e
Parasuramandckap <parasuramanadckap@gmail.com>
2023-03-03 14:42:26 +0530

    emi

--- index.js
+++ index.js
@@ -5,56 +5,69 @@ const peroid = document.querySelector(".ini")
 const calculate = document.querySelector(".cal")
 const select = document.querySelector("#select")
 //console.log(select.value)
+let button = document.querySelectorAll("button")
+
+let mnbtn = document.querySelector("#month")
+let yrbtn = document.querySelector("#year")
+
+mnbtn.addEventListener("click",()=>{
+    mnbtn.classList.add("show")
+    yrbtn.classList.remove("show")
+})
+
+yrbtn.addEventListener("click",()=>{
+    yrbtn.classList.add("show")
+    mnbtn.classList.remove("show")
+})
+
+for(let i=0;i<button.length;i++){
+    button[i].addEventListener("click",(e)=>{
+        if(e.target.matches("#month")){
+            let p = peroid.value *12;
+            peroid.value=p
+
+        }
+        else if(e.target.matches("#year")){
+            let p = peroid.value
+            if ( p > 1) {
+                let k =peroid.value/12
+                peroid.value = k
+                
+            }
+         
+        }
+    })
+}
 calculate.addEventListener("click", () => {
     if (loan.value !== " " && rate.value !== "" && peroid.value !== "") {
-        if (select.value == "Month") {
             //console.log(select.value)
+           
+
+        if(mnbtn.classList.contains("show")){
             let amount = loan.value;
             let rate_percentage = rate.value / 1200;
             let period_value = peroid.value
+            let emi = amount * rate_percentage * (Math.pow((1 + rate_percentage), period_value) / (Math.pow((1 + rate_percentage), period_value) - 1));
+            let real_value = Math.round(emi)
+            let original_emi = Math.round(emi * period_value)
+            let original_rate = Math.round(original_emi - amount);
+            document.querySelector(".loan-emi").innerText = `${real_value}`
+            document.querySelector(".total-payment").innerText = `${original_emi}`
+            document.querySelector(".intersert-display").innerText = `${original_rate}`
 
-            if(amount && rate_percentage && period_value){
-                let emi = amount * rate_percentage * (Math.pow((1 + rate_percentage), period_value) / (Math.pow((1 + rate_percentage), period_value) - 1));
-                let real_value = Math.round(emi)
-                let original_emi = Math.round(emi * period_value)
-                let original_rate = Math.round(original_emi - amount);
-                document.querySelector(".loan-emi").innerText = `${real_value}`
-                document.querySelector(".total-payment").innerText = `${original_emi}`
-                document.querySelector(".intersert-display").innerText = `${original_rate}`
-    
-                // loan.value = ""
-                // rate.value = ""
-                // peroid.value = ""
-            }
-
-
-        }
-        else if (select.value == "Year") {
+        }else{
+            let period_value =  peroid.value * 12
             let amount = loan.value;
             let rate_percentage = rate.value / 1200;
-            let period_value = peroid.value * 12;
 
-            if(amount && rate_percentage && period_value){
-                let emi = amount * rate_percentage * (Math.pow((1 + rate_percentage), period_value) / (Math.pow((1 + rate_percentage), period_value) - 1));
-               // alert(emi)
-                let real_value = Math.round(emi)
-                // console.log(real_value)
-                let original_emi = Math.round(emi * period_value)
-                let original_rate = Math.round(original_emi - amount);
-                document.querySelector(".loan-emi").innerText = `${real_value}`
-                document.querySelector(".total-payment").innerText = `${original_emi}`
-                document.querySelector(".intersert-display").innerText = `${original_rate}`
-    
-                // loan.value = ""
-                // rate.value = ""
-                // peroid.value = ""
-            }
+            let emi = amount * rate_percentage * (Math.pow((1 + rate_percentage), period_value) / (Math.pow((1 + rate_percentage), period_value) - 1));
+            let real_value = Math.round(emi)
+            let original_emi = Math.round(emi * period_value)
+            let original_rate = Math.round(original_emi - amount);
+            document.querySelector(".loan-emi").innerText = `${real_value}`
+            document.querySelector(".total-payment").innerText = `${original_emi}`
+            document.querySelector(".intersert-display").innerText = `${original_rate}`        }
 
-
-        }
-        else {
-            alert("please select month or year")
-        }
 
     }
     else {
